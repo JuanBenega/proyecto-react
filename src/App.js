287ed27ec49ded1a0a254ea897d43ae30e8387c1
juan benega <jmbenega@gmail.com>
2022-08-31 20:00:07 -0300

    merge al main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import ItemList from './components/ItemList/ItemList';
-
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
-      <ItemList />
+      <ItemList /> 
     </>
   )
 }
