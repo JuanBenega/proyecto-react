d19a6b4de5801b512cab009ef14335606acad714
juan benega <jmbenega@gmail.com>
2022-09-14 06:22:17 -0300

    modificacion readme.md

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<ItemListContainer />} />
             <Route path='detail/:id' element={<ItemDetailContainer />} />
             <Route path='login' element={<h2>LOGIN DE USUARIO</h2>} />
-            <Route path='category/:cat' element={<ItemListContainer />} />
+            <Route path='category/:catId' element={<ItemListContainer />} />
         </Routes>
       </BrowserRouter>
     </>
