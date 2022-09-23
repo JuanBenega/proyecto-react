2ddad7f814d391b2dd71cf85416315e06d0c2853
Benega <XJMB05@domainba.com>
2022-09-23 12:01:08 -0300

    add loader to ItemListContainer & ItemDetailContainer

--- src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -4,16 +4,19 @@ import data from '../../components/mockData';
 import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { useParams } from "react-router-dom";
+import  RingLoader  from "react-spinners/RingLoader";
 
 
 
 const ItemDetailContainer = () => {
     const { id } = useParams();
     const [item, setItem] = useState([])
+    const [load, setLoad] = useState(false)
 
     useEffect(() => {
         getItem.then((response) => {
-            setItem(response[0])
+            setItem(response[0]);
+            setLoad(true);
         })
             .catch((error) => {
                 console.log(error);
@@ -29,23 +32,25 @@ const ItemDetailContainer = () => {
 
     return (
         <>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8 my-3 p-2 detail">
-                        <h2 className='text-center'>Detalle de Item</h2>
-                        <ItemDetail
-                            name={item.name}
-                            description={item.description}
-                            img={item.img}
-                            price={item.price}
-                            item={item.item}
-                        />
+            {load === false
+                ? <div className="loader"><RingLoader color="#67b967" size={200} /></div>
+                : <div className="container-fluid">
+                    <div className="row my-5">
+                        <div className="col-md-2"></div>
+                        <div className="col-md-8 my-3 p-2 detail">
+                            <h2 className='text-center'>Detalle de Item</h2>
+                            <ItemDetail
+                                name={item.name}
+                                description={item.description}
+                                img={item.img}
+                                price={item.price}
+                                item={item.item}
+                            />
+                        </div>
+                        <div className="col-md-2"></div>
                     </div>
-                    <div className="col-md-2"></div>
                 </div>
-            </div>
-
+            }
         </>
     )
 }
